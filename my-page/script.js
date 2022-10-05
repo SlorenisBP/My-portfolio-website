e7307908bb44a7c5ecc1ffc603c88b993fe21797
SlorenisBP <blaciusek1997@gmail.com>
2022-10-05 12:35:08 +0200

    stick-ed navbar further improvements

--- my-page/script.js
+++ my-page/script.js
@@ -7,11 +7,22 @@
 // });
 
 const navbar = document.querySelector(".navbar");
+const navbarOffsetTop = navbar.offsetTop;
+const sections = document.querySelectorAll('section');
+const navbarLinks = document.querySelectorAll('.navbar-link')
+
 
 window.addEventListener("scroll", () => {
-    if (window.pageYOffset >= navbar.offsetTop){
+    if (window.pageYOffset >= navbar.offsetTop) {
         // console.log('sticky');
         navbar.classList.add('sticky')
-    } else{ navbar.classList.remove('sticky');
+    } else {
+        navbar.classList.remove('sticky');
     }
-})
+    sections.forEach(sections => {
+        if (window.pageYOffset >= section.offsetTop -10){
+
+        }
+
+            })
+});
